ee11674e5c830e339e7a6ae615de11340bed080e
Gustavo Franke <franke.gustavo@gmail.com>
2015-05-26 22:08:47 +0100

    adding code for basic command chaining

---
+++ src/main/groovy/creatingdsls/CommandChain.groovy
@@ -0,0 +1,28 @@
+package creatingdsls
+
+def (forward, left, then, fast, right) =
+		[
+			'forward',
+			'left',
+			'',
+			'fast',
+			'right'
+		]
+def move(dir) {
+	println "moving $dir"
+	this
+}
+def and(then) {
+	this
+}
+def turn(dir) {
+	println "turning $dir"
+	this
+}
+def jump(speed, dir) {
+	println "jumping $speed and $dir"
+	this
+}
+
+move forward and then turn left
+jump fast, forward and then turn right
