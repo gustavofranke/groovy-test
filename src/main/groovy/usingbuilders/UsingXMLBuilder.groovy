9372db51213eea6d5d4c07afc2445b700acbd732
Gustavo Franke <franke.gustavo@gmail.com>
2015-05-26 23:37:01 +0100

    adding basic xml builder example

---
+++ src/main/groovy/usingbuilders/UsingXMLBuilder.groovy
@@ -0,0 +1,33 @@
+package usingbuilders
+
+bldr = new groovy.xml.MarkupBuilder()
+bldr.languages {
+	language(name: 'C++') { author('Stroustrup')}
+	language(name: 'Java') { author('Gosling')}
+	language(name: 'Lisp') { author('McCarthy')}
+}
+
+//println bldr
+//-------------------------------------------------
+langs = ['C++' : 'Stroustrup', 'Java' : 'Gosling', 'Lisp' : 'McCarthy']
+writer = new StringWriter()
+bldr = new groovy.xml.MarkupBuilder(writer)
+bldr.languages {
+	langs.each { key, value ->
+		language(name: key) { author (value) }
+	}
+}
+//println writer
+//-------------------------------------------------
+langs = ['C++' : 'Stroustrup', 'Java' : 'Gosling', 'Lisp' : 'McCarthy']
+xmlDocument = new groovy.xml.StreamingMarkupBuilder().bind {
+	mkp.xmlDeclaration()
+	mkp.declareNamespace(computer: "Computer")
+	languages {
+		comment << "Created using StreamingMarkupBuilder"
+		langs.each { key, value ->
+			computer.language(name: key) { author (value) }
+		}
+	}
+}
+println xmlDocument
